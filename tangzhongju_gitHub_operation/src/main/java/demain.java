cb0f16f43045066e6ab52266e400cfd50038e68f
tangzhongju <1547012663@qq.com>
2023-03-24 10:47:43 +0800

    123

---
+++ tangzhongju_gitHub_operation/src/main/java/demain.java
@@ -0,0 +1,19 @@
+import lombok.Data;
+
+/**
+ * @BelongsProject: first_git_demo
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: tangzhongju
+ * @CreateTime: 2023-03-24  10:45
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@Data
+public class demain {
+
+    private String name;
+
+    private Integer id;
+
+    private  Double money;
+}
